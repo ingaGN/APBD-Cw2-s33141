e51e1ccdb0d1b6660154876bd5eddcc156e980f2
Inga Gadomska-Nguyen <s33141@pjwstk.edu.pl>
2026-03-25 23:44:24 +0100

    dodanie klasy sprzet

--- projekt/projekt/Sprzet.cs
+++ projekt/projekt/Sprzet.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace projekt;
+
+public class Sprzet
+{
+    private int id = 0;
+    
+    public int Id { get ; }
+    public string Nazwa { get; set; }
+    public string Status { get; set; }
+
+    public Sprzet(string nazwa)
+    {
+        Id = id++;
+        Nazwa = nazwa;
+        Status = "dostepny";
+    }
+}
