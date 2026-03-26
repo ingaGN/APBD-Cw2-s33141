9b8b8e7b2b41f60e7983b5db21aae74aaaed4de2
Inga Gadomska-Nguyen <s33141@pjwstk.edu.pl>
2026-03-26 01:05:32 +0100

    dodanie klasy Rental

---
+++ projekt/projekt/Rental.cs
@@ -0,0 +1,27 @@
+﻿namespace projekt;
+
+public class Rental
+{
+    public Uzytkownik Uzytkownik { get;}
+    public Sprzet Sprzet { get;}
+    
+    public DateTime DataWypozyczenia { get; }
+    public DateTime DataZwrotuDue { get; }
+    public DateTime? DataZwrotu { get; set; }
+    
+    public double Kara { get; set; }
+    
+    public bool CzyZwrocona => DataZwrotu.HasValue;
+    public bool CzyZwroconaWTerminie => DataZwrotu.HasValue && DataZwrotu <= DataZwrotuDue ;
+
+    public Rental(Uzytkownik uzytkownik, Sprzet sprzet, DateTime dataWypozyczenia, DateTime dataZwrotuDue)
+    {
+        this.Uzytkownik = uzytkownik;
+        this.Sprzet = sprzet;
+        this.DataWypozyczenia = dataWypozyczenia;
+        this.DataZwrotuDue = dataZwrotuDue;
+    }
+    
+    
+    
+}
